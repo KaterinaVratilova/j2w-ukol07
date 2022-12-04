9cc0403afadaaa423d7c13ec8742e2a70c3a4be9
Katerina Vratilova <katerina.vratilova@gmail.com>
2022-12-04 21:07:40 +0100

    řešení dú

--- src/main/java/cz/czechitas/java2webapps/ukol7/controller/PostController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol7/controller/PostController.java
@@ -2,6 +2,7 @@ package cz.czechitas.java2webapps.ukol7.controller;
 
 import cz.czechitas.java2webapps.ukol7.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,15 +20,15 @@ public class PostController {
 
   // zobrazení úvodní stránky se seznamem postů
   @GetMapping("/")
-  public ModelAndView listOfPosts() {
-    return new ModelAndView("posts")
+  public ModelAndView listOfPosts(Pageable pageable) {
+    return new ModelAndView("index")
       .addObject("posts", postService.list());
   }
 
   // zobrazení jednoho postu
 @GetMapping("/post/{slug}")
-  public ModelAndView onePost(@PathVariable(value = "slug") String slug) {
-  return new ModelAndView("onepost")
+  public ModelAndView onePost(@PathVariable String slug) {
+  return new ModelAndView("post")
     .addObject("post", postService.singlePost(slug));
   }
 
